2e5a0f945b94353886c5a9783fafa0116f65a954
h-sompura <sompharshi123@gmail.com>
2024-08-03 17:55:26 +0530

    wip(slider): add images for mood labels

--- components/Slider.tsx
+++ components/Slider.tsx
@@ -1,6 +1,7 @@
+import React, { useState, useMemo } from "react";
+
 import Slider from "@react-native-community/slider";
-import { useState } from "react";
-import { View, Text } from "react-native";
+import { View, Text, Image, StyleSheet } from "react-native";
 
 const CustomSlider = () => {
   const textLabels = [
@@ -13,18 +14,35 @@ const CustomSlider = () => {
     "very pleasant",
   ];
 
-  const calculateLabelIndex = (value: number) => {
-    const labelCount = textLabels.length;
-    const index = Math.round((labelCount - 1) * value);
-    return Math.max(0, Math.min(index, labelCount - 1));
-  };
+  const images = [
+    require("../assets/image1.png"),
+    require("../assets/image2.png"),
+    require("../assets/image3.png"),
+    require("../assets/image4.png"),
+    require("../assets/image5.png"),
+    require("../assets/image6.png"),
+    require("../assets/image7.png"),
+  ];
+
+  const calculateLabelIndex = useMemo(() => {
+    return (value: number) => {
+      const labelCount = textLabels.length;
+      const index = Math.round((labelCount - 1) * value);
+      return {
+        labelIndex: Math.max(0, Math.min(index, labelCount - 1)),
+        imageIndex: Math.max(0, Math.min(index, images.length - 1)),
+      };
+    };
+  }, [textLabels, images]);
 
   const [value, setValue] = useState(0.5);
 
-  const labelIndex = calculateLabelIndex(value);
+  const { labelIndex, imageIndex } = calculateLabelIndex(value);
   const label = textLabels[labelIndex];
+  const image = images[imageIndex];
   return (
     <View>
+      <Image source={image} style={styles.image} />
       <Text
         style={{
           textAlign: "center",
@@ -33,6 +51,7 @@ const CustomSlider = () => {
           fontSize: 30,
           fontWeight: "700",
           textTransform: "capitalize",
+          marginTop: 24,
         }}
       >
         {label}
@@ -43,6 +62,7 @@ const CustomSlider = () => {
           borderRadius: 30,
           paddingVertical: 6,
           paddingHorizontal: 12,
+          marginTop: 24,
         }}
       >
         <Slider
@@ -59,5 +79,20 @@ const CustomSlider = () => {
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  image: {
+    width: 200,
+    height: 200,
+    alignSelf: "center",
+    margin: 8,
+    padding: 8,
+    objectFit: "contain",
+  },
+});
 
 export default CustomSlider;

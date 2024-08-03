7569cb210028d32614b5285e40ea7eba117d0603
h-sompura <sompharshi123@gmail.com>
2024-08-03 17:16:44 +0530

    wip(home): add text headings and next button

--- App.tsx
+++ App.tsx
@@ -1,9 +1,10 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, Pressable } from "react-native";
 import CustomSlider from "./components/Slider";
 export default function App() {
   return (
     <View style={styles.container}>
+      <Text style={styles.heading}>Choose how you're feeling right now</Text>
       <View>
         <CustomSlider />
         <View
@@ -35,7 +36,9 @@ export default function App() {
           </Text>
         </View>
       </View>
-
+      <Pressable style={styles.button}>
+        <Text style={styles.buttonText}>Next</Text>
+      </Pressable>
       <StatusBar style="auto" />
     </View>
   );
@@ -46,6 +49,22 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#000",
     alignItems: "center",
-    justifyContent: "center",
+    justifyContent: "space-evenly",
+  },
+  heading: {
+    color: "white",
+    fontSize: 32,
+    textAlign: "center",
+    width: 300,
+  },
+  button: {
+    backgroundColor: "#FAFAF9",
+    paddingHorizontal: 160,
+    paddingVertical: 20,
+    borderRadius: 36,
+  },
+  buttonText: {
+    fontSize: 20,
+    fontWeight: "bold",
   },
 });
